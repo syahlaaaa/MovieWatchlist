16ab1fea385012fdb8ea09df864ddc63f2f5b6b3
syahlaaaa <dliyasyahla789@gmail.com>
2024-02-27 15:46:30 +0800

    watchlist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import   "./App.css";
 
-function App() {
+export default function MovieWatchListCalaaApp() {
+  const [movies, setMovies] = useState([]);
+
+  function handleAddMovie(movie) {
+    setMovies((movies) => [...movies, movie]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <MovieForm onAddMovie={handleAddMovie} />
+      <MovieList movies={movies} />
+      <footer movies={movies} />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>Movie WatchList 🍿</h1>;
+}
+
+function MovieForm({ onAddMovie }) {
+  const [title, setTitle] = useState("");
+  const [watched,setWatched]= useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!title) return;
+
+    const newMovie = { title, watched, id: Date.now() };
+    onAddMovie(newMovie);
+
+    setTitle("");
+    setWatched(false);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>add a Movie Here </h3>
+      <input
+      type="text"
+      placeholder="Enter your movie title"
+      value={title}
+      onChange={(e) => setTitle(e.target.value)}
+      />
+      <button>add</button>
+    </form>
+  );
+}
+
+function MovieList({movies,onDeleteMovie }) {
+  return (
+    <div className="list">
+      <ul>
+        {movies.map((movie) =>(
+          <MovieItem movie={movie} key={movie.id} onDeleteMovie={onDeleteMovie}
+           />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function MovieItem({ movie }) {
+  return (
+    <li>
+      <span style={MovieForm.watched ? {textDecoration: "line-through"} : {}}>
+        {movie.title}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function footer({ movies }) {
+  const watchedMovies = movies.filter((movie) => movie.watched);
+  const percentWatched = (watchedMovies.lenght / movies.lenght) * 100 || 0;
+
+  return (
+    <footer className="stats">
+      <em>
+        you have {movies.lenght} movies in your list, and { watchedMovies.lenght}{" "}
+        ({percentWatched.toFixed(2)}%) watched.
+      </em>
+    </footer>
+  );
+}
